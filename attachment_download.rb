c7fb0b91ff4ec930693288c40758495b1d465a1d
James - The Original Master Branch <james@ilockhart.com>
2017-04-30 21:51:21 -0400

    changing to upload to google drive instead of pulling locally

--- attachment_download.rb
+++ attachment_download.rb
@@ -1,8 +1,12 @@
 require 'mail'
 require 'yaml'
+require 'google_drive'
+
+PDF_MIME_TYPE = 'application/pdf'
 
 config = YAML.load_file('config/config.yml')
-
+session = GoogleDrive::Session.from_config("config/google_config.json")
+exit
 Mail.defaults do
   retriever_method :imap, :address    => config["mail_config"]["address"],
                           :port       => config["mail_config"]["port"],
@@ -19,18 +23,20 @@ def create_filename(mail, filename)
   filename.gsub(/[^0-9a-z\._-]/i, '')
 end
 
-def download_attachment(mail, download_path)
+def upload_attachment(mail, download_path, session)
   mail.attachments.each do |attachment|
+    mime_type = attachment.mime_type
     filename = create_filename(mail, attachment.filename)
 
     begin
-      File.open("#{download_path}/#{filename}", "w+b", 0644) do |f|
-        f.write attachment.body.decoded
-        puts "Wrote file to #{download_path} with name #{filename}"
-        return true
-      end
+      raise "invalid filetype #{mime_type} can only accept #{PDF_MIME_TYPE}" if mime_type != PDF_MIME_TYPE
+
+      session.upload_from_file(attachment.filename, filename, convert: false)
+      puts "Wrote file to google drive with name #{filename}"
+
+      return true
     rescue => e
-      puts "Unable to save data for #{filename} because #{e.message}"
+      puts "Unable to upload to google drive for #{filename} because #{e.message}"
       return false
     end
   end
@@ -41,5 +47,5 @@ def mark_as_read(uid, imap)
 end
 
 Mail.find(keys: ['NOT','SEEN']) do |mail, imap, uid|
-  mark_as_read(uid, imap) if download_attachment(mail, config["download_path"])
+  mark_as_read(uid, imap) if upload_attachment(mail, config["download_path"], session)
 end
